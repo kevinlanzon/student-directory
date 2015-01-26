6b4d0143ff240fc0ab295ef8fc62502eba03f111
Kevin Lanzon <kevinlanzon@gmail.com>
2015-01-26 20:10:51 +0000

    Refactored into three methods

--- directory.rb
+++ directory.rb
@@ -1,17 +1,32 @@
-student_count = 9
-#first we print the list of students
-puts "The students of my cohort at Makers Academy"
-puts "--------------"
-puts "Kevin Lanzon"
-puts "Roger Moore"
-puts "James Bond"
-puts "Sean Connery"
-puts "John Smith"
-puts "Burt Reynolds"
-puts "Sharon Stone"
-puts "Albert Einstein"
-puts "Charlie Chaplin"
-# finally, we print the total
-puts "Overall, we have #{student_count} great students"
+# let's put all students into an array
+students = [
+  "Kevin Lanzon",
+  "Roger Moore",
+  "James Bond",
+  "Sean Connery",
+  "John Smith",
+  "Burt Reynolds",
+  "Sharon Stone",
+  "Albert Einstein",
+  "Charlie Chaplin"
+]
 
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "--------------"
+end
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.length} great students"
+end
+# nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
 
