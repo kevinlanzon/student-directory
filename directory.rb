ff60e393b66cbf5064fbb59f1cb640c60b3e1ef2
Kevin Lanzon <kevinlanzon@gmail.com>
2015-01-27 19:28:54 +0000

    Refactored the code. Added methods

--- directory.rb
+++ directory.rb
@@ -1,63 +1,70 @@
+  @students = [] # empty array accessible to all methods
+
 def print_header
   puts "The students of my cohort at Makers Academy"
   puts "--------------"
 end
 
-def print(students)
-  students.each_with_index do |student, index|
+def print_students_list
+  @students.each_with_index do |student, index|
     puts "#{index + 1}: #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.length} great students"
+def print_footer
+  puts "Overall, we have #{@students.length} great students"
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {:name => name, :cohort => :February}
-    puts "Now we have #{students.length} students"
+    @students << {:name => name, :cohort => :February}
+    puts "Now we have #{@students.length} students"
     # get another name from the user
     name = gets.chomp
   end
-  # return the array of students
-  students
 end
 
-def interactive_menu
-  students = []
-  loop do
-  # 1. print menu and ask user what to do
+def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "9. Exit" # 9 as we will be adding more items
-  # 2. read input and save it into a variable
-  selection = gets.chomp
-  # 3. do what user has asked
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
   case selection
-  when "1"
-    students = input_students
+    when "1"
+      input_students
   when "2"
-    print_header
-    print(students)
-    print_footer(students)
+      show_students
   when "9"
     exit # this will cause the program to terminate
   else
-    puts "Sorry, I don't know what you meant, please try again"
+    puts "Sorry, I don't know what you mean, please try again"
    end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
   end
 end
 
 interactive_menu
+
+
 
 
 
